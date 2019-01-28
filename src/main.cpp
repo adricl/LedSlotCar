43c2fd6bc680ed6ddc6cf30400866a27350b2dea
Adric <adric.landman@gmail.com>
2019-01-28 21:29:56 +1100

    Improved code, cleaned up comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 // How many leds in your strip?
 #define NUM_LEDS 180
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN 2
 #define CLOCK_PIN 3
 #define LED_TYPE    APA102
@@ -15,45 +12,54 @@
 #define CYCLE_SPEED 50
 struct CAR {
 	int8_t player;
-	int32_t position;
+	float position;
 	int8_t colourH;
 	int8_t colourS;
 	int8_t colourV;
-	int8_t gradientV;
 };
 
 #define CAR_SIZE 7
 #define TRACK_SIZE 10
+#define CAR_NUMBER 4
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-CAR cars[2];
+CAR cars[CAR_NUMBER];
 
 void setup() { 
 	Serial.begin(57600);
 	Serial.println("resetting");
 	FastLED.addLeds<LED_TYPE,DATA_PIN,CLOCK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-	FastLED.setBrightness(255);
+	FastLED.setBrightness(127);
+	FastLED.setDither( 255 );
 
-	//Setting Up Player 1
+	//Car Setup for Colours and Numbers
 	cars[0].position = 0;
 	cars[0].player = 1;
 	cars[0].colourH = 0;
 	cars[0].colourS = 255;
-	cars[0].colourV = 255;
-	cars[0].gradientV = 255;
+	cars[0].colourV = 200;
 
 	cars[1].position = 0;
 	cars[1].player = 2;
 	cars[1].colourH = 96;
 	cars[1].colourS = 255;
-	cars[1].colourV = 255;
-	cars[1].gradientV = 255;
+	cars[1].colourV = 200;
 	
+	cars[2].position = 0;
+	cars[2].player = 3;
+	cars[2].colourH = 192;
+	cars[2].colourS = 255;
+	cars[2].colourV = 255;
+
+	cars[3].position = 0;
+	cars[3].player = 4;
+	cars[3].colourH = 32;
+	cars[3].colourS = 255;
+	cars[3].colourV = 255;
 }
 
-//void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
-
+//Wraping the track and dealing with -ve numbers that the tails can generate
 int getStripPos(int distance) { 
 	if (distance < 0) {
 		return NUM_LEDS + distance;
@@ -62,6 +68,7 @@ int getStripPos(int distance) {
 	}
 }
 
+//Clears leds
 void blankLeds() {
 	for(int i = 0; i < NUM_LEDS; i++){
 		leds[i] = CRGB::Black;
@@ -69,19 +76,16 @@ void blankLeds() {
 	FastLED.show(); 
 }
 
-
+//Places car on track with tail
+//Tail is caculated as the v in hsv fading over the tail length
 void placeCar(int carNum) {
 	CAR car = cars[carNum];
-	int pos = car.position;
-	uint8_t vChange = (int)(car.gradientV / CAR_SIZE);
+	uint8_t vChange = (uint8_t) car.colourV / CAR_SIZE;
 	CHSV colour = CHSV(car.colourH, car.colourS, car.colourV);
-	leds[getStripPos(pos)] += colour;
+	leds[getStripPos(car.position)] += colour;
 	for(int i = 1; i < CAR_SIZE; i++){
 		colour.setHSV(colour.h, colour.s ,colour.v - vChange); 
-		leds[getStripPos(pos - i)] += colour;
-		Serial.printf("Car: %d ", carNum);
-		Serial.printf("POS: %d ", i);
-		Serial.printf("Colour: %d ", (int)colour.v);
+		leds[getStripPos(car.position - i)] += colour;
 	}
 }
 
@@ -92,23 +96,28 @@ void blankCars(int carNum){
 	}
 }
 
+
+//Main race loop
 void race(){
 	int laps = 10;
 
 	while (cars[0].position < (laps * NUM_LEDS) || cars[1].position < (laps * NUM_LEDS) )
 	{
 		//display car 0
-		placeCar(0);
-		placeCar(1);
-		
+		for(int i = 0; i < CAR_NUMBER; i++)	{
+			placeCar(i);	
+		}
+
 		FastLED.show();
 
-		blankCars(0);
-		blankCars(1);
+		for(int i = 0; i < CAR_NUMBER; i++)	{
+			blankCars(i);	
+		}
 
 		cars[0].position ++;
-		cars[1].position ++;
-		cars[1].position ++;
+		cars[1].position += 2;
+		cars[2].position += .5;
+		cars[3].position += 1.5;
 
 		delay(CYCLE_SPEED);
 	}
