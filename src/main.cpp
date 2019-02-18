b72a1b021b223f7ea67b88d5a9806e76a4fe140a
Adric <adric.landman@gmail.com>
2019-02-19 00:00:15 +1100

    Added Crash code and some refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,10 @@ struct CAR {
   int8_t colourS;
   int8_t colourV;
   int pin;
+  int crashTimer;
 };
 
-#define CAR_SIZE 7
+#define CAR_SIZE 8
 #define TRACK_SIZE 50
 #define CAR_NUMBER 4
 // Define the array of leds
@@ -42,7 +43,8 @@ void setup() {
   cars[0].colourH = 0;
   cars[0].colourS = 255;
   cars[0].colourV = 200;
-  cars[0].pin = 0;
+  cars[0].pin = -1; //normally 0
+  cars[0].crashTimer = 0;
   
   //blue
   cars[1].position = 0;
@@ -50,13 +52,17 @@ void setup() {
   cars[1].colourH = 96;
   cars[1].colourS = 255;
   cars[1].colourV = 200;
-  
+  cars[1].pin = -1;
+  cars[1].crashTimer = 0;
+
   //yellow
   cars[2].position = 0;
   cars[2].player = 3;
   cars[2].colourH = 192;
   cars[2].colourS = 255;
   cars[2].colourV = 255;
+  cars[2].pin = -1;
+  cars[2].crashTimer = 0;
 
   //aqua
   cars[3].position = 0;
@@ -64,6 +70,8 @@ void setup() {
   cars[3].colourH = 32;
   cars[3].colourS = 255;
   cars[3].colourV = 255;
+  cars[3].pin = -1;
+  cars[3].crashTimer = 0;
 }
 
 //Wraping the track and dealing with -ve numbers that the tails can generate
@@ -75,66 +83,95 @@ int getStripPos(int distance) {
   }
 }
 
-//Clears leds
-void blankLeds() {
+//Blanks all leds
+void blankAllLeds() {
   for(int i = 0; i < NUM_LEDS; i++){
     leds[i] = CRGB::Black;
   }
   FastLED.show(); 
 }
 
-//Converts analog to 0 to 1
-float getCarSpeed(int carNum){
-  return (float)analogRead(cars[carNum].pin)/1023;
-}
-
 //Places car on track with tail
 //Tail is caculated as the v in hsv fading over the tail length
-void placeCar(int carNum) {
+void renderNormalCar(CAR car, bool clear){
+    if (clear){
+      for(int i = 0; i < CAR_SIZE; i++){
+        leds[getStripPos(car.position - i)] = CRGB::Black;
+      }
+    }
+    else {
+      uint8_t vChange = (uint8_t) car.colourV / (CAR_SIZE - 1);
+      
+      float percent = car.position - (int)car.position;
+      leds[getStripPos(car.position)] += CHSV(car.colourH, car.colourS, car.colourV * percent);
+      
+      CHSV colour = CHSV(car.colourH, car.colourS, car.colourV);
+      leds[getStripPos(car.position - 1)] += colour;
+      for(int i = 2; i < CAR_SIZE; i++){
+        colour.setHSV(colour.h, colour.s ,colour.v - vChange); 
+        leds[getStripPos(car.position - i)] += colour;
+      }
+    }
+}
+
+void renderCrashedCar(CAR car, bool clear){
+  if (!clear && ((car.crashTimer % 5) == 0)) {
+    renderNormalCar(car, clear);
+  }
+  car.crashTimer --;
+}
+
+void renderCar(int carNum, bool clear) {
   CAR car = cars[carNum];
-  uint8_t vChange = (uint8_t) car.colourV / CAR_SIZE;
-  CHSV colour = CHSV(car.colourH, car.colourS, car.colourV);
-  leds[getStripPos(car.position)] += colour;
-  for(int i = 1; i < CAR_SIZE; i++){
-    colour.setHSV(colour.h, colour.s ,colour.v - vChange); 
-    leds[getStripPos(car.position - i)] += colour;
+
+  if (car.crashTimer > 0){
+    renderCrashedCar(car, clear);
+  }
+  else {
+    renderNormalCar(car, clear);
   }
 }
 
-void blankCars(int carNum){
+void updateCarPosition(int carNum, double updateAmount){
   CAR car = cars[carNum];
-  for(int i = 0; i < CAR_SIZE; i++){
-    leds[getStripPos(car.position - i)] = CRGB::Black;
+  if (car.crashTimer > 0){
+    return;
+  } 
+  else if(car.pin > 0){
+    cars[carNum].position += (float)analogRead(car.pin)/1023;
+  }
+  else {
+    cars[carNum].position += updateAmount;
   }
 }
 
 
 //Main race loop
 void race(){
-  int laps = 10;
+  //int laps = 10;
 
   //display car 0
   for(int i = 0; i < CAR_NUMBER; i++) {
-    placeCar(i);  
+    renderCar(i, false);  
   }
 
   FastLED.show();
 
   for(int i = 0; i < CAR_NUMBER; i++) {
-    blankCars(i); 
+    renderCar(i, true); 
   }
 
-  cars[0].position += getCarSpeed(0);
-  cars[1].position += .75;
-  cars[2].position += .25;
-  cars[3].position += .2;
+  updateCarPosition(0, .5);
+  updateCarPosition(1, .75);
+  updateCarPosition(2, .25);
+  updateCarPosition(3, .2);
 
-    delay(CYCLE_SPEED);
+  delay(CYCLE_SPEED);
 }
 
 
 void loop(){
-  blankLeds();
+  blankAllLeds();
   race();
 
 }
